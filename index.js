323162e34009aeb6101e0623ed75309ef7222705
Gavin Veitch <gavintveitch@gmail.com>
2022-09-22 09:46:16 -0600

    commit tree 2f6196a94b669964aecb7f40b9ee8c7819c58b3b

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let texts = []
+    for (let count = 0; count < names.length; count++) {
+        texts.push(`Thank you, ${names[count]}, for the wonderful surprise gift!`)
+
+    }
+
+    return texts
+
+}
+
+function countDown(num) {
+    for (let count =0; count <= num; count ++){
+        console.log(num - count)
+    }
+
+}
+
